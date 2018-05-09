e51fb56ac6d1d59ef7532b31d4a55966815f4cee
Pauli Beat, IT141 <beat.pauli@post.ch>
2018-05-09 11:39:06 +0200

    update_mailpiece_attributes added

---
+++ src/ts/update_mailpiece_attributes.ts
@@ -0,0 +1,15 @@
+export interface UpdateMailpieceAttributes {
+    iood?: boolean;
+    weight?: number;
+    indiciaValue?: string;
+    indiciaType?: string;
+    sortBin?: number;
+    idtagPrintStatus?: boolean;
+    code?: string;
+    finalCoded?: boolean;
+    mailpieceId: string;
+    sourceId?: string;
+    scannerId?: string;
+    feederId?: string;
+    sortplanId?: string;
+}
